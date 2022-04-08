60db1afd933ca234b8c3588e81d73e95829572c6
Sahidev1 <theoddrogue@gmail.com>
2022-04-08 14:35:20 +0200

    workflow test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Dummy App
+          Dummy App!!! 
         </h1>
       </header>
     </div>
